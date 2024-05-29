efd638196a1d2164525443b2c2c605082ccab063
av7388605503 <av84002499@gmail.com>
2024-05-29 07:39:51 +0530

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ dotenv.config();
 server.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    'https://kanpurpanal.vercel.app/'
+    'https://kanpurpanal.vercel.app'
   );
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', '*');
